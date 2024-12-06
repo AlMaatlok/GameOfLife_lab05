80b4cec2ed4ab752333c740913441e99cea4cf1d
Alicja Matłok <163751160+AlMaatlok@users.noreply.github.com>
2024-12-06 08:33:17 +0100

    poprawki

--- src/MainGameOfLife.java
+++ src/MainGameOfLife.java
@@ -11,12 +11,12 @@ public class MainGameOfLife {
         System.out.println("please provide absolute path to config file: ");
         Scanner scanner = new Scanner(System.in);
         String path = scanner.nextLine();
+        FileHandler Fh = new FileHandler(path);
+        Configuration config = Fh.getConfiguration();
+
         System.out.println("provide number of threads you want to run: ");
         Scanner scanner2 = new Scanner(System.in);
         int numberOfThreads = scanner2.nextInt();
-
-        FileHandler Fh = new FileHandler(path);
-        Configuration config = Fh.getConfiguration();
 
         LogicHandler logic = new LogicHandler(config);
         logic.printWorkingThreads(numberOfThreads, config);
